8b9decbcc4eb701fcd3e2fc64418ef2d7fd4ea94
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-22 02:51:35 +0530

    Add: Category Filters and mobile FIlters

--- app/(routes)/category/[categoryId]/page.tsx
+++ app/(routes)/category/[categoryId]/page.tsx
@@ -6,6 +6,9 @@ import Billboard from "@/components/Billboard";
 import Container from "@/components/ui/Container";
 import { FC } from "react";
 import Filter from "./components/Filter";
+import NoResults from "@/components/ui/NoResults";
+import ProductCard from "@/components/ui/ProductCard";
+import MobileFilters from "./components/MobileFIlters";
 
 export const revalidate = 0
 
@@ -17,7 +20,6 @@ export interface CategoryPageProps {
     colorId: string;
     sizeId: string;
   }
-
 }
 
 const CategoryPage: FC<CategoryPageProps> = async ({
@@ -40,10 +42,18 @@ const CategoryPage: FC<CategoryPageProps> = async ({
         <Billboard data={category.billboard} />
         <div className="px-4 sm:px-6 lg:px-8 pb-24">
           <div className="lg:grid lg:grid-cols-5 lg:gap-x-8">
-            {/* Add mobile filters */}
+            <MobileFilters sizes={sizes} colors={colors} />
             <div className="hidden lg:block">
               <Filter valueKey="sizeId" name="Sizes" data={sizes} />
               <Filter valueKey="colorId" name="Colors" data={colors} />
+            </div>
+            <div className="mt-6 lg:col-span-4 lg:mt-0">
+              {products.length === 0 && <NoResults />}
+              <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-4">
+                {products.map((item) => (
+                  <ProductCard key={item.id} data={item} />
+                ))}
+              </div>
             </div>
           </div>
         </div>
