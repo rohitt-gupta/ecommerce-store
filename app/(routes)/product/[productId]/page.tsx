7bc8a68d14d00c80d09ac0b6a150c2d513936933
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-21 21:48:40 +0530

    Add: Infir Page

--- app/(routes)/product/[productId]/page.tsx
+++ app/(routes)/product/[productId]/page.tsx
@@ -1,5 +1,6 @@
 import getProduct from "@/actions/getProduct";
 import getProducts from "@/actions/getProducts";
+import Info from "@/components/Info";
 import ProductList from "@/components/ProductList";
 import Gallery from "@/components/gallery";
 import Container from "@/components/ui/Container";
@@ -22,12 +23,12 @@ const ProductPage: FC<ProductPageProps> = async ({
     <div className="bg-white">
       <Container>
         <div className="px-4 py-10 sm:px-6 lg:px-8">
-          <div className="lg:grid lg:grid-cols-2 lg:items-center lg:gap-x-8">
+          <div className="lg:grid lg:grid-cols-2 lg:items-start lg:gap-x-8">
             {/* Gallery */}
             <Gallery images={product.images} />
             <div className="mt-10 px-4 sm:mt-16 sm:px-0 lg:mt-0">
               {/* Info */}
-              info
+              <Info data={product} />
             </div>
           </div>
           <hr className="my-10" />
