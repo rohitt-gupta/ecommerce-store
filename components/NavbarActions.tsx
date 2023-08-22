0eccf4c3f5ea9adcb133731b837937feac90e363
rohitGupta2022 <pta.rohit28@gmail.com>
2023-08-23 00:35:07 +0530

    Add: Cart Page

--- components/NavbarActions.tsx
+++ components/NavbarActions.tsx
@@ -4,6 +4,7 @@ import { ShoppingBag } from 'lucide-react'
 import React, { useEffect, useState } from 'react'
 import Button from './ui/Button'
 import useCart from '@/hooks/useCart'
+import { useRouter } from 'next/navigation'
 
 const NavbarActions = () => {
   //  No HYDRATION ERROR
@@ -14,14 +15,14 @@ const NavbarActions = () => {
   }, [])
 
   const cart = useCart()
-
+  const router = useRouter()
   if (!isMounted) return null
 
 
 
   return (
     <div className='ml-auto flex items-center gap-x-4'>
-      <Button className="flex items-center rounded-full bg-black px-4 py-2">
+      <Button onClick={() => router.push("/cart")} className="flex items-center rounded-full bg-black px-4 py-2">
         <ShoppingBag size={20} color='white' />
         <span className='ml-2 text-sm font-medium text-white'>{cart.items.length}</span>
       </Button>
